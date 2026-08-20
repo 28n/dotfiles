6631aa42e0a4ec42b98a7d13bcff57bb2571743d
Nick Habelsberger <nick@habelsberger.com>
2026-08-20 13:10:21 +0200

    Add Tokyo tmux workspace

--- .custom/bash/main.sh
+++ .custom/bash/main.sh
@@ -5,3 +5,8 @@ eval "$(zoxide init bash)"
 if [[ -f "$HOME/.custom/bash/aliases.sh" ]]; then
   source "$HOME/.custom/bash/aliases.sh"
 fi
+
+if [[ -f "$HOME/.custom/bash/tmux.sh" ]]; then
+  # shellcheck source=tmux.sh
+  source "$HOME/.custom/bash/tmux.sh"
+fi
